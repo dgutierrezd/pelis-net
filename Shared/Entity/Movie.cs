8f4fe17a7b36b74f2c0752230e95781b929de4cd
Daniel Gutierrez <danielgudu22@gmail.com>
2021-09-18 11:50:10 -0500

    first commit

---
+++ Shared/Entity/Movie.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Proy1.Shared.Entity
+{
+    public class Movie
+    {
+        public string Name{get; set; }
+        public string Sinopsis{get; set; }
+        public DateTime Premiere{get; set; }
+    }
+}
